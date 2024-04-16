0ad85e1dc489736f53d6fa0813ae453a9997a85c
djfksdl <djfksdl@naver.com>
2024-04-16 21:02:32 +0900

    아이템리스트

--- src/main/java/com/javaex/service/ItemService.java
+++ src/main/java/com/javaex/service/ItemService.java
@@ -14,13 +14,31 @@ public class ItemService {
 	@Autowired
 	private ItemDao itemDao;
 	
-	//카테고리별 아이템 리스트 불러오기
+	//대분류별 아이템 리스트 불러오기
 	public List<ProductVo> getItemList(int no) {
 		System.out.println("ItemService.getItemList");
 		
 		List<ProductVo> ItemList = itemDao.selectItemList(no);
 		
 		return ItemList;
+	}
+	
+	//대분류별 소분류 목록 불러오기 
+	public List<ProductVo> getScList(int no) {
+		System.out.println("ItemService.getScList");
+		
+		List<ProductVo> scList = itemDao.selectScList(no);
+		
+		return scList;
+	}
+		
+	//소분류별 아이템 리스트 받기 
+	public List<ProductVo> getIList(int no) {
+		System.out.println("ItemService.getIList");
+		
+		List<ProductVo> iList = itemDao.selectIList(no);
+		
+		return iList;
 	}
 	
 }
